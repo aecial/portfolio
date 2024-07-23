2c9fc33d89f4a697f3b4c0dd47762839657bc6a9
Teddy Pascual <teddylayson2000@gmail.com>
2024-07-23 17:27:55 +0800

    try deleting export configs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  distDir: "docs",
-  basePath: "/docs",
-};
+const nextConfig = {};
 
 module.exports = nextConfig;

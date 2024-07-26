40b7b93b8f839b82b53b44fcb05263e52ae92e45
Teddy Pascual <teddylayson2000@gmail.com>
2024-07-26 12:10:59 +0800

    add position shift for mobile

--- app/components/SkillTitle.jsx
+++ app/components/SkillTitle.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SkillTitle = ({ skill }) => {
-  return <h3 className="text-3xl text-primary luxurious">{skill}</h3>;
+  return (
+    <h3 className="text-3xl text-center lg:text-start text-primary luxurious">
+      {skill}
+    </h3>
+  );
 };
 
 export default SkillTitle;

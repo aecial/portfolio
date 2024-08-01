e8123cb78f2c751a790032e9c6b0e663e63ed540
Teddy Pascual <teddylayson2000@gmail.com>
2024-08-01 11:52:25 +0800

    add project contents

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -8,16 +8,8 @@ const projects = () => {
       <PageTitle title={"My Projects"} />
       <div className="flex flex-col lg:flex-row gap-4 flex-wrap justify-center items-center pb-4">
         <ProjectContainer
-          src={"/projects/p1.jpg"}
-          projectTitle={"Miralles Resort"}
-          projectDescription={"Website for Miralles Resort SEO optimized"}
-          technologies={["html", "css", "javascript", "tailwind"]}
-          site={true}
-          siteLink={"https://www.mirallesresort.com/"}
-        />
-        <ProjectContainer
           src={"/projects/p2.jpg"}
-          projectTitle={"LGU-Gerona Queueing System"}
+          projectTitle={"Queueing System"}
           projectDescription={"Queueing System for LGU Gerona"}
           technologies={[
             "html",
@@ -26,6 +18,58 @@ const projects = () => {
             "tailwind",
             "react",
             "express",
+            "mysql",
+          ]}
+          demo={true}
+        />
+        <ProjectContainer
+          src={"/projects/p4.jpg"}
+          projectTitle={"Civil Registry Information System"}
+          projectDescription={"CRIS for LGU Gerona"}
+          technologies={[
+            "html",
+            "css",
+            "javascript",
+            "bootstrap",
+            "php",
+            "mysql",
+          ]}
+          demo={true}
+        />
+        <ProjectContainer
+          src={"/projects/p3.jpg"}
+          projectTitle={"Agrikultur'App"}
+          projectDescription={"Online Agricultural Produce Auction Platform"}
+          technologies={[
+            "html",
+            "css",
+            "javascript",
+            "bootstrap",
+            "Laravel",
+            "mysql",
+          ]}
+          site={true}
+          siteLink={"https://www.mirallesresort.com/"}
+        />
+        <ProjectContainer
+          src={"/projects/p1.jpg"}
+          projectTitle={"Miralles Resort"}
+          projectDescription={"Website for Miralles Resort SEO optimized"}
+          technologies={["html", "css", "javascript", "tailwind"]}
+          site={true}
+          siteLink={"https://www.mirallesresort.com/"}
+        />
+
+        <ProjectContainer
+          src={"/projects/p5.jpg"}
+          projectTitle={"POS"}
+          projectDescription={"Web-based Point of Sales System"}
+          technologies={[
+            "html",
+            "css",
+            "javascript",
+            "tailwind",
+            "php",
             "mysql",
           ]}
           demo={true}
